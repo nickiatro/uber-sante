feab44bb72fb567a56b7cf253ac8f33bed92e54c
moumaa <31826851+moumaa@users.noreply.github.com>
2019-03-11 17:33:45 -0400

    re-added migration tables for nurse and physician

---
+++ app/Nurse.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class Nurse extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'accessId', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+    ];
+
+    public $timestamps = true;
+}
---
+++ app/Physician.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class Physician extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'firstName','lastName', 'physicianNumber', 'password', 'specialty', 'city',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+    ];
+
+    public $timestamps = true;
+}
