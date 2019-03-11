cccb4a2d3fb7ac3c25fc1630445fe02e32bdb282
moumaa <31826851+moumaa@users.noreply.github.com>
2019-03-11 16:32:24 -0400

    add admin account to list of pre-determined accounts

--- database/migrations/2019_03_10_081253_create_users_table.php
+++ database/migrations/2019_03_10_081253_create_users_table.php
@@ -31,6 +31,22 @@ class CreateUsersTable extends Migration
             $table->integer('admin_privilege')->default(0);
             $table->integer('logged_in')->default(0);
         });
+
+        DB::table('users')->insert([
+            'id' => 1100,
+            'email' => "admin@admin.com",
+            'password' => "adminadmin",
+            'healthCard' => "adminadmin",
+            'birthday' => "01/01/2019",
+            'gender' => "other",
+            'phone' => "1010101010",
+            'country' => "Canada",
+            'province' => "Quebec",
+            'postalCode' => "h1h1h1",
+            'city' => "montreal",
+            'street' => "admin street",
+            'admin_privilege' => 1,
+        ]);
     }
 
     /**
