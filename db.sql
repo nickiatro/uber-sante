f7ed87b78ba61e6679e230bf2722effd84599b03
meetaz <mshbli75@hotmail.com>
2019-03-11 20:46:03 -0400

    add doctor availability and clinic tables

--- db.sql
+++ db.sql
@@ -9,10 +9,35 @@ CREATE TABLE `doctor` (
   `dob` date NOT NULL,
   `experience` int(11) NOT NULL,
   `specialization` varchar(30) NOT NULL,
-  `contact` bigint(20) NOT NULL,
+  `phone` bigint(20) NOT NULL,
+  `ext` bigint(20) NOT NULL,
   `address` varchar(100) NOT NULL,
   `username` varchar(30) NOT NULL,
   `password` varchar(30) NOT NULL,
   `city` varchar(30) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+--
+-- Create table `doctor_availability`
+--
+
+CREATE TABLE `doctor_availability` (
+  `clinic_id` int(11) NOT NULL,
+  `doc_id` int(11) NOT NULL,
+  `day` varchar(50) NOT NULL,
+  `start_time` time NOT NULL,
+  `end_time` time NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- create table `clinic`
+--
+
+CREATE TABLE `clinic` (
+  `clinic_id` int(11) NOT NULL,
+  `clinic_name` varchar(30) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `town` varchar(30) NOT NULL,
+  `phone` varchar(30) NOT NULL,
+  `contact` bigint(20) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
