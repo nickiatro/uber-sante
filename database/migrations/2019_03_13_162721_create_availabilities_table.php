b877df63a13eea50bc3136caab7485b4f0a21ece
Paul <paul.lane083@gmail.com>
2019-03-13 14:11:47 -0400

    Create all basic needed models for scheduling and appointments

---
+++ database/migrations/2019_03_13_162721_create_availabilities_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAvailabilitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('availabilities', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('physician_id');
+            $table->dateTime('start_time');
+            $table->integer('duration');
+            $table->timestamps();
+
+            $table->foreign('physician_id')->references('id')->on('physicians');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('availabilities');
+    }
+}
---
+++ database/migrations/2019_03_13_175411_create_clinics_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClinicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clinics', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('name');
+            $table->timestamps();
+        });
+
+        DB::table('clinics')->insert([
+            'name' => 'Uber Sante Clinic'
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clinics');
+    }
+}
