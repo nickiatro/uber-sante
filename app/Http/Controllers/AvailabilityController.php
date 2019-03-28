eeab69be5ac26cd7b12805f93484a0008170dea2
Arrow105 <sean.how0@gmail.com>
2019-03-28 13:26:04 -0400

    Renamed aMap to avMap to avoid confusion with appointment mapper

--- app/Http/Controllers/AvailabilityController.php
+++ app/Http/Controllers/AvailabilityController.php
@@ -6,36 +6,36 @@ use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
-use App\DataMappers\availability_mapper;
+use App\DatavMappers\availability_mapper;
 use App\Availability;
 
 class AvailabilityController extends Controller
 {    
     public static function showAvailability($start_time)
     {
-      $aMap = new availability_mapper();
-      $availabilities = $aMap->showAvailability($start_time);
+      $avMap = new availability_mapper();
+      $availabilities = $avMap->showAvailability($start_time);
       return $availabilities;
     }
     
     public function updateAvailability(Availability $availability)
     {
-      $aMap = new availability_mapper();
-      $aMap->updateAvailability($availability);  
+      $avMap = new availability_mapper();
+      $avMap->updateAvailability($availability);  
       return redirect()->back();
     }
     
     public function addAvailability(Availability $availability)
     {
-      $aMap = new availability_mapper();
-      $aMap->addAvailability($availability);    
+      $avMap = new availability_mapper();
+      $avMap->addAvailability($availability);    
       return redirect()->back();
     }
     
     public function removeAvailability($physician_id, $start_time)
     {
-      $aMap = new availability_mapper();
-      $aMap->removeAvailability($physician_id, $start_time);    
+      $avMap = new availability_mapper();
+      $avMap->removeAvailability($physician_id, $start_time);    
       return redirect()->back();
     }
 
