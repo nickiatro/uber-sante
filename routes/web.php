99c21a31b9905e5122c57b7d6ff0b7a2caf18ca4
Arrow105 <sean.how0@gmail.com>
2019-03-27 21:33:48 -0400

    Fixed login route for Physicians

--- routes/web.php
+++ routes/web.php
@@ -33,7 +33,7 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/login/physician', 'Auth\LoginController@showLoginForm');
-Route::post('/login/physician', 'Auth\LoginController@validateLogin')->name('login.physician');
+Route::post('/login/physician', 'Auth\LoginController@validateLoginPhysicians')->name('login.physician');
 
 Route::get('/login/nurse', 'Auth\LoginController@showLoginForm');
 Route::post('/login/nurse', 'Auth\LoginController@validateLoginNurses')->name('login.nurse');
