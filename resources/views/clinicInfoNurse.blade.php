08fae2be76a5f64328f302664b48aa5df5f560b1
moumaa <31826851+moumaa@users.noreply.github.com>
2019-04-12 10:53:22 -0400

    finished clinic interface #55

--- resources/views/clinicInfoNurse.blade.php
+++ resources/views/clinicInfoNurse.blade.php
@@ -124,6 +124,344 @@ if (array_key_exists("assign-button", $_POST)) {
         </div>
     </div>
 </div>
+
+<?php
+
+$countPhysician1 = DB::table('clinic')->where('physicianNumber','!=',null)->where('clinic_id','=',1)->count();
+$countNurse1 = DB::table('clinic')->where('accessId','!=',null)->where('clinic_id','=',1)->count();
+$countroom1 = DB::table('appointments')->where('clinic_id', '=', 1)->where('room_Id','!=',null)->count();
+$appointmentClinic1 = DB::table('appointments')->where('clinic_id', '=', 1)->get();
+
+$countPhysician2 = DB::table('clinic')->where('physicianNumber','!=',null)->where('clinic_id','=',2)->count();
+$countNurse2 = DB::table('clinic')->where('accessId','!=',null)->where('clinic_id','=',2)->count();
+$countroom2 = DB::table('appointments')->where('clinic_id', '=', 2)->where('room_Id','!=',null)->count();
+$appointmentClinic2 = DB::table('appointments')->where('clinic_id', '=', 2)->get();
+
+$countPhysician3 = DB::table('clinic')->where('physicianNumber','!=',null)->where('clinic_id','=',3)->count();
+$countNurse3 = DB::table('clinic')->where('accessId','!=',null)->where('clinic_id','=',3)->count();
+$countroom3 = DB::table('appointments')->where('clinic_id', '=', 3)->where('room_Id','!=',null)->count();
+$appointmentClinic3 = DB::table('appointments')->where('clinic_id', '=', 3)->get();
+
+$countPhysician4 = DB::table('clinic')->where('physicianNumber','!=',null)->where('clinic_id','=',4)->count();
+$countNurse4 = DB::table('clinic')->where('accessId','!=',null)->where('clinic_id','=',4)->count();
+$countroom4 = DB::table('appointments')->where('clinic_id', '=', 4)->where('room_Id','!=',null)->count();
+$appointmentClinic4 = DB::table('appointments')->where('clinic_id', '=', 4)->get();
+
+$countPhysician5 = DB::table('clinic')->where('physicianNumber','!=',null)->where('clinic_id','=',5)->count();
+$countNurse5 = DB::table('clinic')->where('accessId','!=',null)->where('clinic_id','=',5)->count();
+$countroom5 = DB::table('appointments')->where('clinic_id', '=', 5)->where('room_Id','!=',null)->count();
+$appointmentClinic5 = DB::table('appointments')->where('clinic_id', '=', 5)->get();
+
+?>
+
+<div class="container" style= "padding:20px;">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header flip">{{ __('Medistat Clinic') }}</div>
+
+                    <div class="card-body panel" style="display: none">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="clinic_id" class="col-md-4 col-form-label text-md-right">{{ __('Number of physicians:') }}</label>
+
+                            <div class="col-md-6">
+                            <?php echo $countPhysician1 ?>
+                            </div>
+
+                        </div>
+                        <div class="form-group row">
+                            <label for="clinic_id" class="col-md-4 col-form-label text-md-right">{{ __('Number of nurses:') }}</label>
+
+                            <div class="col-md-6">
+                            <?php echo $countNurse1 ?>
+                            </div>
+                            
+                        </div>
+                        <div class="form-group row">
+                            <label for="clinic_id" class="col-md-4 col-form-label text-md-right">{{ __('Number of rooms used:') }}</label>
+
+                            <div class="col-md-6">
+                            <?php echo $countroom1 ?>
+                            </div>
+                            
+                        </div>
+
+                        <table class='table table-bordered'>
+                            <tr style="text-align:center">
+
+                                <th>Clinic ID</th>
+                                <th>Date & Time</th>
+                                <th>Duration</th>
+                                <th>Patient Health Card Number</th>
+                                <th>Physician Number</th>
+                                <th>Room Number</th>
+                            </tr>
+                            @foreach($appointmentClinic1 as $appointment)
+                            <tr>
+
+                                <td>{{$appointment->clinic_id}}</td>
+                                <td>{{$appointment->start_time}}</td>
+                                <td>{{$appointment->duration}}</td>
+                                <td>{{$appointment->healthCard}}</td>
+                                <td>{{$appointment->physicianNumber}}</td>
+                                <td>{{$appointment->room_id}}</td>
+
+                                </tr>
+                            @endforeach
+                        </table>
+                    </div>
+                </div>
+            <div>
+        </div>
+    </div>
+</div>
+<div class="container" style= "padding:20px;">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header flip">{{ __('Super Clinic') }}</div>
+
+                    <div class="card-body panel" style="display: none">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="clinic_id" class="col-md-4 col-form-label text-md-right">{{ __('Number of physicians:') }}</label>
+
+                            <div class="col-md-6">
+                            <?php echo $countPhysician2 ?>
+                            </div>
+
+                        </div>
+                        <div class="form-group row">
+                            <label for="clinic_id" class="col-md-4 col-form-label text-md-right">{{ __('Number of nurses:') }}</label>
+
+                            <div class="col-md-6">
+                            <?php echo $countNurse2 ?>
+                            </div>
+                            <div class="form-group row">
+                            <label for="clinic_id" class="col-md-4 col-form-label text-md-right">{{ __('Number of rooms used:') }}</label>
+
+                            <div class="col-md-6">
+                            <?php echo $countroom2 ?>
+                            </div>
+                            </div>
+                            <table class='table table-bordered'>
+                            <tr style="text-align:center">
+
+                                <th>Clinic ID</th>
+                                <th>Date & Time</th>
+                                <th>Duration</th>
+                                <th>Patient Health Card Number</th>
+                                <th>Physician Number</th>
+                                <th>Room Number</th>
+                            </tr>
+                            @foreach($appointmentClinic2 as $appointment)
+                            <tr>
+
+                                <td>{{$appointment->clinic_id}}</td>
+                                <td>{{$appointment->start_time}}</td>
+                                <td>{{$appointment->duration}}</td>
+                                <td>{{$appointment->healthCard}}</td>
+                                <td>{{$appointment->physicianNumber}}</td>
+                                <td>{{$appointment->room_id}}</td>
+
+                                </tr>
+                            @endforeach
+                        </table>
+                            
+                        </div>
+                    </div>
+                </div>
+            <div>
+        </div>
+    </div>
+</div>
+<div class="container" style= "padding:20px;">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header flip">{{ __('Alpha Clinic') }}</div>
+
+                    <div class="card-body panel" style="display: none">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="clinic_id" class="col-md-4 col-form-label text-md-right">{{ __('Number of physicians:') }}</label>
+
+                            <div class="col-md-6">
+                            <?php echo $countPhysician3 ?>
+                            </div>
+
+                        </div>
+                        <div class="form-group row">
+                            <label for="clinic_id" class="col-md-4 col-form-label text-md-right">{{ __('Number of nurses:') }}</label>
+
+                            <div class="col-md-6">
+                            <?php echo $countNurse3 ?>
+                            </div>
+                            
+                        </div>
+                        <div class="form-group row">
+                            <label for="clinic_id" class="col-md-4 col-form-label text-md-right">{{ __('Number of rooms used:') }}</label>
+
+                            <div class="col-md-6">
+                            <?php echo $countroom3 ?>
+                            </div>
+                            
+                        </div>
+
+                        <table class='table table-bordered'>
+                            <tr style="text-align:center">
+
+                                <th>Clinic ID</th>
+                                <th>Date & Time</th>
+                                <th>Duration</th>
+                                <th>Patient Health Card Number</th>
+                                <th>Physician Number</th>
+                                <th>Room Number</th>
+                            </tr>
+                            @foreach($appointmentClinic3 as $appointment)
+                            <tr>
+
+                                <td>{{$appointment->clinic_id}}</td>
+                                <td>{{$appointment->start_time}}</td>
+                                <td>{{$appointment->duration}}</td>
+                                <td>{{$appointment->healthCard}}</td>
+                                <td>{{$appointment->physicianNumber}}</td>
+                                <td>{{$appointment->room_id}}</td>
+
+                                </tr>
+                            @endforeach
+                        </table>
+                    </div>
+                </div>
+            <div>
+        </div>
+    </div>
+</div>
+<div class="container" style= "padding:20px;">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header flip">{{ __('Diamant Clinic') }}</div>
+
+                    <div class="card-body panel" style="display: none">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="clinic_id" class="col-md-4 col-form-label text-md-right">{{ __('Number of physicians:') }}</label>
+
+                            <div class="col-md-6">
+                            <?php echo $countPhysician4 ?>
+                            </div>
+
+                        </div>
+                        <div class="form-group row">
+                            <label for="clinic_id" class="col-md-4 col-form-label text-md-right">{{ __('Number of nurses:') }}</label>
+
+                            <div class="col-md-6">
+                            <?php echo $countNurse4 ?>
+                            </div>
+                            </div>
+                        <div class="form-group row">
+                            <label for="clinic_id" class="col-md-4 col-form-label text-md-right">{{ __('Number of rooms used:') }}</label>
+
+                            <div class="col-md-6">
+                            <?php echo $countroom4 ?>
+                            </div>
+                            
+                        </div>
+                        <table class='table table-bordered'>
+                            <tr style="text-align:center">
+
+                                <th>Clinic ID</th>
+                                <th>Date & Time</th>
+                                <th>Duration</th>
+                                <th>Patient Health Card Number</th>
+                                <th>Physician Number</th>
+                                <th>Room Number</th>
+                            </tr>
+                            @foreach($appointmentClinic4 as $appointment)
+                            <tr>
+
+                                <td>{{$appointment->clinic_id}}</td>
+                                <td>{{$appointment->start_time}}</td>
+                                <td>{{$appointment->duration}}</td>
+                                <td>{{$appointment->healthCard}}</td>
+                                <td>{{$appointment->physicianNumber}}</td>
+                                <td>{{$appointment->room_id}}</td>
+
+                                </tr>
+                            @endforeach
+                        </table>
+                    </div>
+                </div>
+            <div>
+        </div>
+    </div>
+</div>
+<div class="container" style= "padding:20px;">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header flip">{{ __('MediSys Clinic') }}</div>
+
+                    <div class="card-body panel" style="display: none">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="clinic_id" class="col-md-4 col-form-label text-md-right">{{ __('Number of physicians:') }}</label>
+
+                            <div class="col-md-6">
+                            <?php echo $countPhysician5 ?>
+                            </div>
+
+                        </div>
+                        <div class="form-group row">
+                            <label for="clinic_id" class="col-md-4 col-form-label text-md-right">{{ __('Number of nurses:') }}</label>
+
+                            <div class="col-md-6">
+                            <?php echo $countNurse5 ?>
+                            </div>
+                            </div>
+                        <div class="form-group row">
+                            <label for="clinic_id" class="col-md-4 col-form-label text-md-right">{{ __('Number of rooms used:') }}</label>
+
+                            <div class="col-md-6">
+                            <?php echo $countroom5 ?>
+                            </div>
+                        </div>
+                        <table class='table table-bordered'>
+                            <tr style="text-align:center">
+
+                                <th>Clinic ID</th>
+                                <th>Date & Time</th>
+                                <th>Duration</th>
+                                <th>Patient Health Card Number</th>
+                                <th>Physician Number</th>
+                                <th>Room Number</th>
+                            </tr>
+                            @foreach($appointmentClinic5 as $appointment)
+                            <tr>
+
+                                <td>{{$appointment->clinic_id}}</td>
+                                <td>{{$appointment->start_time}}</td>
+                                <td>{{$appointment->duration}}</td>
+                                <td>{{$appointment->healthCard}}</td>
+                                <td>{{$appointment->physicianNumber}}</td>
+                                <td>{{$appointment->room_id}}</td>
+
+                                </tr>
+                            @endforeach
+                        </table>
+                    </div>
+                </div>
+            <div>
+        </div>
+    </div>
+</div>
 </body>
 @endsection
 </html>
