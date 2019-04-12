d8bef2fe5e406a45363df059f8d5241eb3ff86c5
RazvanSoos <razvan.soos@gmail.com>
2019-04-12 10:30:33 -0400

    Fixed the time not being displayed in the database

--- resources/views/createAvailability.blade.php
+++ resources/views/createAvailability.blade.php
@@ -30,6 +30,7 @@ if(!isset($_SESSION)){
 <?php
 $typeErr = $dateErr = $timeErr= "";
 $walkIn = $annual  = $date = "";
+$time = "";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if (($_POST["date"]) == null) {
@@ -70,9 +71,6 @@ else if ($annual == "selected") {
     $duration = 60;
 }
 
-$time = "";
-
-
 if (array_key_exists("add-to-cart-button", $_POST)) {
     DB::table('availabilities')->insert(  [
         'physicianNumber' => Auth::guard('physician')->user()->physicianNumber,
@@ -82,8 +80,10 @@ if (array_key_exists("add-to-cart-button", $_POST)) {
         'updated_at' => date('Y-m-d H:i:s')]);
     header("Location: /myAvailabilities");
     exit;
+	
 }
 ?>
+
 @section('content')
 <h1 id="header" class="text-center">
     Create Availabilities
@@ -97,7 +97,6 @@ if (array_key_exists("add-to-cart-button", $_POST)) {
 
     <form method= "post" action="{{route('createAvailability')}}">
         @csrf
-       
         <div class="form-group">
         <label for="date">Choose an Appointment Date</label>
             <input class="form-control" type="date" id="date" name="date" 
@@ -109,18 +108,17 @@ if (array_key_exists("add-to-cart-button", $_POST)) {
 				<?php echo $dateErr;?>
 			</span>
         </div>
-
         <div>
         <label for="times">Select Appointment Time</label>
             <input class="form-control" value="08:00" type="time" id="times" name="times" 
 				min="08:00"
 				max="16:00"			
 				value= "<?php echo $time;?>" required
-			>
-			
-            <span class="error" style="color:red;"><?php echo $timeErr;?></span>
+			>		
+            <span class="error" style="color:red;">
+				<?php echo $timeErr;?>
+			</span>
         </div>
-
         <div class="form-group">
           <label for="appointment-type">Select the Appointment Type</label>
           <select class="form-control" id="appointment-type" name="appointment-type" required>
